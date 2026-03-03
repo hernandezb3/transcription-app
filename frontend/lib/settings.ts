2b6e1433ce4de38fc0136f6de4a3260c9909a01a
hernandezb3 <brittney.hernandez@uconn.edu>
2026-03-03 15:16:13 -0500

    change env variable name

--- frontend/lib/settings.ts
+++ frontend/lib/settings.ts
@@ -1,7 +1,7 @@
 import localSettings from "@/settings.local.json";
 import liveSettings from "@/settings.live.json";
 
-const appEnv = (process.env.NEXT_PUBLIC_APP_ENV ?? "local").toLowerCase();
+const appEnv = (process.env.APP_ENVIRONMENT ?? "local").toLowerCase();
 
 const settingsByEnv = {
 	local: localSettings,
